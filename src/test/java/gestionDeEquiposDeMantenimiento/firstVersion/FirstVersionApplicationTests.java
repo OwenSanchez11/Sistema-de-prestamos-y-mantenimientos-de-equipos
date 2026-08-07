f38fd3382f1059968a4dcaff82d134c21fda5cad
Owen Sanchez <owensanchez1104@gmail.com>
2026-08-07 15:57:49 -0500

    se agregó activeProfiles a firsVersionAppTests

--- src/test/java/gestionDeEquiposDeMantenimiento/firstVersion/FirstVersionApplicationTests.java
+++ src/test/java/gestionDeEquiposDeMantenimiento/firstVersion/FirstVersionApplicationTests.java
@@ -2,8 +2,10 @@ package gestionDeEquiposDeMantenimiento.firstVersion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FirstVersionApplicationTests {
 
 	@Test
